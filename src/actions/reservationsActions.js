b77b3260fd7d06bf00db43895484110dbd0875f7
github username <rvtingling@yahoo.com>
2021-04-11 02:56:11 -0400

    created fetch reservations

---
+++ src/actions/reservationsActions.js
@@ -0,0 +1,21 @@
+
+
+export const fetchReservations = () => {
+    return dispatch => {
+        fetch('http://localhost:3000/reservations')
+        .then(resp => resp.json())
+        .then(reservations => dispatch({ type: 'FETCH_RESERVATIONS', payload: reservations}))
+    }
+}
+
+// export const addGrad = (grad) => {
+//     return dispatch => {
+//         fetch('http://127.0.0.1:3000/grads', {
+//             method: 'POST',
+//             body: JSON.stringify(grad),
+//             headers: { 'Content-Type': 'application/json'}
+//         })
+//         .then(resp => resp.json())
+//         .then(grad => dispatch({ type: 'ADD_GRAD', payload: grad}))
+//     }
+// }
