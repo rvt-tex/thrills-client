d614b8cde9457bf568772d412d1cb498dd05f2da
github username <rvtingling@yahoo.com>
2021-04-11 02:08:36 -0400

    created store and render

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+import App from './components/App'
+import { rootReducer } from './reducers/rootReducer'
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDom.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
